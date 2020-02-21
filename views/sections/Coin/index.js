98672d81031814cf0ede2ce61ad13a08089044bf
Alex <alex@lanp.co>
2020-02-21 14:57:14 +0200

    Refactored coin page

--- views/sections/Coin/index.js
+++ views/sections/Coin/index.js
@@ -7,136 +7,174 @@ import Header from "components/Header";
 
 import type { Props } from "./types";
 
-const Coin = ({
-  data,
-  // error,
-  // progress
-}: Props) => {
-  const {
-    id,
-    logo,
-    name,
-    symbol,
-    category,
-    description,
-    urls,
-    tags,
-    cmc_rank: rank,
-    max_supply: maxSupply,
-    circulating_supply: circulatingSupply,
-    total_supply: totalSupply,
-    quote,
-    pricePeriods
-  } = data;
+const Coin = ({ cryptoInfo }: Props) => (
+  <section className="coin">
+    <Header />
+    {cryptoInfo.progress && <p>Loading...</p>}
+    <img src={cryptoInfo.data?.logo} alt="logo" width="32" height="32" />
+    <p>
+      {cryptoInfo.data?.name}
+      (
+      {cryptoInfo.data?.symbol}
+      )
+    </p>
+    <p>
+      Rank #
+      {/* eslint-disable-next-line camelcase */}
+      {cryptoInfo.data?.cmc_rank}
+    </p>
+    <p>
+      $
+      {cryptoInfo.data?.quote.USD.price.toLocaleString()}
+      {/* eslint-disable-next-line camelcase */}
+      {cryptoInfo.data?.quote.USD.percent_change_24h.toFixed(2)}
+    </p>
+    <br />
+    <p>Market Cap</p>
+    <p>
+      {/* eslint-disable-next-line camelcase */}
+      {cryptoInfo.data?.quote.USD.market_cap.toLocaleString()}
+    </p>
+    <p>
+      {/* eslint-disable-next-line camelcase */}
+      {cryptoInfo.data?.circulating_supply.toLocaleString()}
+      {cryptoInfo.data?.symbol}
+    </p>
+    <br />
+    <p>Volume (24h)</p>
+    <p>
+      $
+      {/* eslint-disable-next-line camelcase */}
+      {cryptoInfo.data?.quote.USD.volume_24h.toLocaleString()}
+    </p>
+    <p>
+      {cryptoInfo.data && (cryptoInfo.data.quote.USD.volume_24h / cryptoInfo.data.quote.USD.price).toLocaleString()}
+      {cryptoInfo.data?.symbol}
+    </p>
+    <br />
+    <div>
+      <p>Circulating Supply</p>
+      {/* eslint-disable-next-line camelcase */}
+      {cryptoInfo.data?.circulating_supply.toLocaleString()}
+      {cryptoInfo.data?.symbol}
+    </div>
+    <br />
+    <div>
+      <p>Max Supply</p>
+      {/* eslint-disable-next-line camelcase */}
+      {cryptoInfo.data?.max_supply.toLocaleString()}
+      {cryptoInfo.data?.symbol}
+    </div>
+    {cryptoInfo.data && <img src={`https://s2.coinmarketcap.com/generated/sparklines/web/7d/usd/${cryptoInfo.data.id}.png`} alt="sparkline" width="164" height="48" />}
+    <br />
+    <p>Links:</p>
+    <p>Website:</p>
+    <ul>
+      {cryptoInfo.data?.urls.website.map(url => <li key={url}><a href={url}>{url}</a></li>)}
+    </ul>
+    <p>Explorer:</p>
+    <ul>
+      {cryptoInfo.data?.urls.explorer.map(url => <li key={url}><a href={url}>{url}</a></li>)}
+    </ul>
+    <p>Message board:</p>
+    <ul>
+      {/* eslint-disable-next-line camelcase */}
+      {cryptoInfo.data?.urls.message_board.map(url => <li key={url}><a href={url}>{url}</a></li>)}
+    </ul>
+    <p>Source code:</p>
+    <ul>
+      {/* eslint-disable-next-line camelcase */}
+      {cryptoInfo.data?.urls.source_code.map(url => <li key={url}><a href={url}>{url}</a></li>)}
+    </ul>
+    <p>Documentation:</p>
+    <ul>
+      {/* eslint-disable-next-line camelcase */}
+      {cryptoInfo.data?.urls.technical_doc.map(url => <li key={url}><a href={url}>{url}</a></li>)}
+    </ul>
+    <br />
+    <p>
+      Category:
+      {cryptoInfo.data?.category}
+      {cryptoInfo.data?.tags.map(tag => <span key={tag}>{tag}</span>)}
+    </p>
+    <br />
+    <p>
+      About
+      {cryptoInfo.data?.name}
+    </p>
+    <p>
+      {cryptoInfo.data?.description}
+    </p>
+    <br />
+    <p>
+      {cryptoInfo.data?.name}
+      statistics
+    </p>
+    <p>
+      {cryptoInfo.data?.name}
+      price $
+      {cryptoInfo.data?.quote.USD.price.toLocaleString()}
+    </p>
+    <p>
+      {cryptoInfo.data?.name}
+      ROI
+      {/* eslint-disable-next-line camelcase */}
+      {cryptoInfo.data?.pricePeriods.all_time.quote.USD.percent_change.toLocaleString()}
+      %
+    </p>
+    <p>
+      Market rank #
+      {/* eslint-disable-next-line camelcase */}
+      {cryptoInfo.data?.cmc_rank}
+    </p>
+    <p>
+      Market cap $
+      {/* eslint-disable-next-line camelcase */}
+      {cryptoInfo.data?.quote.USD.market_cap.toLocaleString()}
+    </p>
+    <p>
+      24 volume $
+      {/* eslint-disable-next-line camelcase */}
+      {cryptoInfo.data?.quote.USD.volume_24h.toLocaleString()}
+    </p>
+    <p>
+      Circulating supply
+      {/* eslint-disable-next-line camelcase */}
+      {cryptoInfo.data?.circulating_supply.toLocaleString()}
+      {cryptoInfo.data?.symbol}
+    </p>
+    <p>
+      Total supply
+      {/* eslint-disable-next-line camelcase */}
+      {cryptoInfo.data?.total_supply.toLocaleString()}
+      {cryptoInfo.data?.symbol}
+    </p>
+    <p>
+      Max supply
+      {/* eslint-disable-next-line camelcase */}
+      {cryptoInfo.data?.max_supply.toLocaleString()}
+      {cryptoInfo.data?.symbol}
+    </p>
+    <p>
+      All time high $
+      {/* eslint-disable-next-line camelcase */}
+      {cryptoInfo.data?.pricePeriods.all_time.quote.USD.high}
+    </p>
+    <p>
+      All time low $
+      {/* eslint-disable-next-line camelcase */}
+      {cryptoInfo.data?.pricePeriods.all_time.quote.USD.low}
+    </p>
 
-  return (
-    <section className="coin">
-      <Header />
-      <img src={logo} alt="logo" width="32" height="32" />
-      <p>{`${name} (${symbol})`}</p>
-      <p>{`Rank #${rank}`}</p>
-      <p>{`$${quote.USD.price.toLocaleString()} ${quote.USD.percent_change_24h.toFixed(2)}`}</p>
-      <br />
-      <p>Market Cap</p>
-      {quote.USD.market_cap && (
-        <p>{`$${quote.USD.market_cap.toLocaleString()}`}</p>
-      )}
-      {circulatingSupply && (
-        <p>{`${circulatingSupply.toLocaleString()} ${symbol}`}</p>
-      )}
-      <br />
-      <p>Volume (24h)</p>
-      <p>{`$${quote.USD.volume_24h.toLocaleString()}`}</p>
-      <p>{`${(quote.USD.volume_24h / quote.USD.price).toLocaleString()} ${symbol}`}</p>
-      <br />
-      {circulatingSupply && (
-        <>
-          <p>Circulating Supply</p>
-          <p>{`${circulatingSupply.toLocaleString()} ${symbol}`}</p>
-        </>
-      )}
-      <br />
-      {maxSupply && (
-        <div>
-          <p>Max Supply</p>
-          <p>{`${maxSupply.toLocaleString()} ${symbol}`}</p>
-        </div>
-      )}
-      <img src={`https://s2.coinmarketcap.com/generated/sparklines/web/7d/usd/${id}.png`} alt="sparkline" width="164" height="48" />
-      <br />
-      <p>Links:</p>
-      <p>Website:</p>
-      {urls.website && urls.website.map(url => (
-        <React.Fragment key={url}>
-          <a href={url}>{url}</a>
-          <br />
-        </React.Fragment>
-      ))}
-      <p>Explorer:</p>
-      {urls.explorer && urls.explorer.map(url => (
-        <React.Fragment key={url}>
-          <a href={url}>{url}</a>
-          <br />
-        </React.Fragment>
-      ))}
-      <p>Message board:</p>
-      {urls.message_board && urls.message_board.map(url => (
-        <React.Fragment key={url}>
-          <a href={url}>{url}</a>
-          <br />
-        </React.Fragment>
-      ))}
-      <p>Source code:</p>
-      {urls.source_code && urls.source_code.map(url => (
-        <React.Fragment key={url}>
-          <a href={url}>{url}</a>
-          <br />
-        </React.Fragment>
-      ))}
-      <p>Documentation:</p>
-      {urls.technical_doc && urls.technical_doc.map(url => (
-        <React.Fragment key={url}>
-          <a href={url}>{url}</a>
-          <br />
-        </React.Fragment>
-      ))}
-      <br />
-      <p>
-        {`Category: ${category} `}
-        {tags && tags.map(tag => <span key={tag}>{tag}</span>)}
-      </p>
-      <br />
-      <p>{`About ${name}`}</p>
-      <p>{description}</p>
-      <br />
-      <p>{`${name} statistics`}</p>
-      <p>{`${name} price $${quote.USD.price.toLocaleString()}`}</p>
-      <p>{`${name} ROI ${pricePeriods.all_time.quote.USD.percent_change.toLocaleString()}%`}</p>
-      <p>{`Market rank #${rank}`}</p>
-      {quote.USD.market_cap && (
-        <p>{`Market cap $${quote.USD.market_cap.toLocaleString()}`}</p>
-      )}
-      <p>{`24 volume $${quote.USD.volume_24h.toLocaleString()}`}</p>
-      {circulatingSupply && (
-        <p>{`Circulating supply ${circulatingSupply.toLocaleString()} ${symbol}`}</p>
-      )}
-      <p>{`Total supply ${totalSupply.toLocaleString()} ${symbol}`}</p>
-      {maxSupply && (
-        <p>{`Max supply ${maxSupply.toLocaleString()} ${symbol}`}</p>
-      )}
-      <p>{`All time high $${pricePeriods.all_time.quote.USD.high}`}</p>
-      <p>{`All time low $${pricePeriods.all_time.quote.USD.low}`}</p>
-    </section>
-  );
-};
+    {cryptoInfo.error && <span className="error">{cryptoInfo.error}</span>}
+  </section>
+);
 
 export default connect(
   ({
-    cryptoInfo: {
-      progress, error, data
-    }
+    cryptoInfo
   }) => ({
-    progress, error, data
+    cryptoInfo
   }),
   null
 )(Coin);
