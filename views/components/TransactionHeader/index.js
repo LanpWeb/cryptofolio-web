f9738a1fd592441776a2491267fac98f89ab3214
Vlad <makoveychyk@gmail.com>
2020-04-10 16:01:20 +0300

    Add transactions

---
+++ views/components/TransactionHeader/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const TransactionHeader = () => (
+  <div className="transaction-header aic jcsb">
+    <div className="p4 fw-medium transaction-header__item aic">
+      <span className="transaction-header__text transaction-header__order">#</span>
+      <span className="transaction-header__text">Coin name</span>
+    </div>
+    <span className="transaction-header__item p3 transaction-header__text">Type</span>
+    <span className="transaction-header__item p3 transaction-header__text">
+      Date
+    </span>
+    <span className="transaction-header__item p3 transaction-header__text">
+      Price
+    </span>
+    <span className="transaction-header__item p3 transaction-header__text">
+      Amount
+    </span>
+    <span className="transaction-header__item p3 transaction-header__text">
+      Total worth
+    </span>
+    <span className="transaction-header__item transaction-header__item_sm" />
+  </div>
+);
+
+export default TransactionHeader;
